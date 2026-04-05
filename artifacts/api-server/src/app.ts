d6cca6e2606b7926ed77c7c70c9619a13f71bbd9
Alperen Arikan <alprn0604@gmail.com>
2026-04-05 19:17:50 +0300

    cors fix

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -28,12 +28,10 @@ app.use(
   }),
 );
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  }),
-);
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 
 app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
